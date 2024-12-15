0c569870aaf454b9a27ea57373eac686d0392e47
Anupam0-0 <anupamchoudhary04@gmail.com>
2024-12-15 09:41:01 +0530

    Fixed menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,8 @@ function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="w-full px-1 h-16 md:h-20 flex items-center justify-between">
+    <div className="w-full px-4 pr-6 sm:px-6 md:px-8 lg:px-16 lx:px-32 2xl:px-64
+     h-16 md:h-20 flex items-center justify-between ">
       {/* LOGO */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="blog logo" w={32} h={32} />
@@ -22,7 +23,7 @@ function Navbar() {
       {/* MOBILE BUTTON */}
       <div className="md:hidden">
         <div
-          className="text-4xl items-center cursor-pointer"
+          className="text-4xl flex items-start cursor-pointer"
           onClick={() => setOpen(!open)}
         >
           {open ? "x" : "≡"}
@@ -30,14 +31,14 @@ function Navbar() {
 
         {/* MOBILE LINK LIST */}
         <div
-          className={`w-full h-screen flex flex-col items-center gap-8 text-lg justify-start py-24 absolute top-16 transition-all ease-in-out duration-1000 ${
-            open ? "right-0 bg-opacity-20" : "-right-full"
+          className={`w-full h-screen overflow-y-hidden flex flex-col items-start gap-8 text-4xl justify-start py-16 md:py-20 px-12 absolute top-16  z-50 transition-all ease-in-out duration-500
+            ${open ? "right-0 bg-blue-50 bg-opacity-95  " : "-right-full hidden"
           }`}
         >
-          <Link to="/">Home</Link>
-          <Link to="/">Trending</Link>
-          <Link to="/">Most Popular</Link>
-          <Link to="/">About</Link>
+          <Link to="/" className="group">Home <hr className="w-0 group-hover:w-full opacity-0 group-hover:opacity-95 border-[2.2px] rounded-full border-slate-800 group-hover:border-blue-600 transition-all ease-in-out duration-[450ms]"/></Link>
+          <Link to="/" className="group">Trending <hr className="w-0 group-hover:w-full opacity-0 group-hover:opacity-95 border-[2.2px] rounded-full border-slate-800 group-hover:border-blue-600 transition-all ease-in-out duration-[450ms]"/></Link>
+          <Link to="/" className="group">Most Popular <hr className="w-0 group-hover:w-full opacity-0 group-hover:opacity-95 border-[2.2px] rounded-full border-slate-800 group-hover:border-blue-600 transition-all ease-in-out duration-[450ms]"/></Link>
+          <Link to="/" className="group">About <hr className="w-0 group-hover:w-full opacity-0 group-hover:opacity-95 border-[2.2px] rounded-full border-slate-800 group-hover:border-blue-600 transition-all ease-in-out duration-[450ms]"/></Link>
           <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login👋
@@ -48,7 +49,8 @@ function Navbar() {
 
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <Link to="/">Home</Link>
+        <Link to="/">Home </Link>
+
         <Link to="/">Trending</Link>
         <Link to="/">Most Popular</Link>
         <Link to="/">About</Link>
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -4,9 +4,11 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div className="px-4 sm:px-6 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+    <div  className="overflow-x-hidden">
         <Navbar /> 
+        <div className="px-4 sm:px-6 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
         <Outlet /> 
+        </div>
     </div>
   );
 };
